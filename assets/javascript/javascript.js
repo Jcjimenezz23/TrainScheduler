78e520b34fb22ab3c31ccfe2ef00c4a2510c382a
Jc <jcjimenezz23@gmail.com>
2017-05-09 19:53:45 -0400

    initialized my firebase configuration, variables and began on click function for submit button

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,30 @@
+//Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBv3ruOzaoY9oJvUzpDFEkOlCq0gEMY63Q",
+    authDomain: "train-scheduler-13294.firebaseapp.com",
+    databaseURL: "https://train-scheduler-13294.firebaseio.com",
+    projectId: "train-scheduler-13294",
+    storageBucket: "train-scheduler-13294.appspot.com",
+    messagingSenderId: "873168447225"
+  };
+firebase.initializeApp(config);
+
+// Create a variable to reference the database
+var database = firebase.database();
+
+//initializing variables
+var trainName = "";
+var destination = "";
+var trainTime = 0;
+var frequency = 0;
+var nextTrain = "";
+var minutesAway = 0;
+
+//capture button click
+$("#addTrain").on("click", function(){
+	// prevent form from trying to submit
+  	event.preventDefault();
+  	
+	trainName = $("#trainInput").val().trim();
+	console.log(trainName);
+})

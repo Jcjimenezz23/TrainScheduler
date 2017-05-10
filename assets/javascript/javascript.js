7abaec7000262d88ae9c480a5b9340266a48d6be
Jc <jcjimenezz23@gmail.com>
2017-05-09 21:07:35 -0400

    incorporated moment.js to calculate next train arrival

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -15,7 +15,7 @@ var database = firebase.database();
 //initializing variables
 var trainName = "";
 var destination = "";
-var trainTime = 0;
+var firstTrain = "";
 var frequency = 0;
 var nextTrain = "";
 var minutesAway = 0;
@@ -28,14 +28,14 @@ $("#addTrain").on("click", function(){
   	//storing user input into variables
 	trainName = $("#trainInput").val().trim();
 	destination = $("#destinationInput").val().trim();
-	trainTime = $("#firsttraintimeInput").val().trim();
+	firstTrain = moment($("#firsttraintimeInput").val().trim(), "HH:mm").subtract(1, "years").format("X");
 	frequency = $("#frequencyInput").val().trim();
-
+console.log(firstTrain);
 	//creating a new variable to store the new train object
 	var newTrain = {
 		name : trainName,
 		place : destination,
-		time : trainTime,
+		time : firstTrain,
 		frequency : frequency
 	}
 
@@ -56,11 +56,25 @@ database.ref().on("child_added", function (childSnapshot, prevChildKey){
 	//create variables to store train information coming back from the database
 	var tName = childSnapshot.val().name;
 	var tDestination = childSnapshot.val().place;
-	var tTime = childSnapshot.val().time;
-	var tfrequency = childSnapshot.val().frequency;
+	var tFirstTrain = childSnapshot.val().time;
+	var tFrequency = childSnapshot.val().frequency;
+
+	//calculating minutes until arrival
+
+	//difference between times
+	var diffTime = moment().diff(moment.unix(tFirstTrain), "minutes");
+
+	//time apart (remainder)
+	var tRemainder = diffTime % tFrequency;
+
+	//minute until arrival
+	var minutesAway = tFrequency - tRemainder;
+
+	//next train
+	var nextTrain = moment().add(minutesAway, "m").format("hh:mm A");
 
 	//populating table with information from database
-	$("#trainTable > tbody").append("<tr><td>"+ tName +"</td> <td>"+ tDestination +"</td> <td>"+ tfrequency +"</td> <td>"+ tTime +"</td></tr>")
+	$("#trainTable > tbody").append("<tr><td>"+ tName +"</td> <td>"+ tDestination +"</td> <td>"+ tFrequency +"</td> <td>"+ nextTrain +"</td> <td>"+ minutesAway +"</td></tr>")
 
 }, function (errorObject){
 	console.log(errorObject.code);

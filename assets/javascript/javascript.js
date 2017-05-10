10ef564188a9f0fbbfcb19a4b9f56920896e8e70
Jc <jcjimenezz23@gmail.com>
2017-05-09 20:15:12 -0400

    on click function stores user input into the database & clear all input fields after

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -24,7 +24,27 @@ var minutesAway = 0;
 $("#addTrain").on("click", function(){
 	// prevent form from trying to submit
   	event.preventDefault();
-  	
+
+  	//storing user input into variables
 	trainName = $("#trainInput").val().trim();
-	console.log(trainName);
+	destination = $("#destinationInput").val().trim();
+	trainTime = $("#firsttraintimeInput").val().trim();
+	frequency = $("#frequencyInput").val().trim();
+
+	//creating a new variable to store the new train object
+	var newTrain = {
+		name : trainName,
+		place : destination,
+		time : trainTime,
+		frequency : frequency
+	}
+
+	//pushing the newTrain object into the database
+	database.ref().push(newTrain);
+
+	//clear all input fields
+	$("#trainInput").val("");
+	$("#destinationInput").val("");
+	$("#firsttraintimeInput").val("");
+	$("#frequencyInput").val("");
 })
